f1952bd4d81968666d3ee733ca280977fac6bd68
Bakutaku <133964557+Bakutaku@users.noreply.github.com>
2024-11-13 08:42:35 +0900

    サイドバー修正
    サイドバーのボタンの大きさなどを修正

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -14,7 +14,7 @@ export default function Sidebar() {
   // アニメーション定義
   const animation : Variants = {
     open : {
-      clipPath: "inset(0% -100% 0% 0% round 10px)",
+      clipPath: "inset(0% -100% 0% -100% round 10px)",
       height: "auto",
       transition:{
         duration: 0.5,
@@ -30,9 +30,11 @@ export default function Sidebar() {
     <nav className="sidebar d-flex align-items-center justify-content-center rounded-pill">
       <div className="pt-2 pb-2">
         {/* ハンバーガーボタン */}
-        <button className="btn my-2" onClick={() => setIsOpen(!isOpen)}>
-          <HamburgerMenu isOpen={isOpen} />
-        </button>
+        <div className="d-flex align-items-center justify-content-center">
+          <button className="btn my-2 d-flex align-items-center justify-content-center" onClick={() => setIsOpen(!isOpen)}  style={{width:50,height:50}}>
+            <HamburgerMenu isOpen={isOpen} />
+          </button>
+        </div>
         {/* 線 */}
         <motion.div animate={isOpen ? "open" : "closed"} variants={animation}>
           <div className="border-bottom border-3 border-black border-opacity-50"></div>
